deabf46db420cfcbaa34009b2a147b492cf0b1f7
João G Tavares <joaogui.t@gmail.com>
2023-07-31 07:44:36 -0300

    Atualizando componentes de time e formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,8 +4,9 @@ import CampoTexto from '../CampoTexto';
 import ListaSupespensa from '../ListaSuspensa';
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
+        '',
         'Programação',
         'Front-End',
         'Data Science',
@@ -22,7 +23,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido', nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado({
+            nome, 
+            cargo, 
+            imagem, 
+            time
+        })
     }
 
     return (
